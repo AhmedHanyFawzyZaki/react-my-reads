2aca00460b607faa188d0f89183e96a3acbc81c0
Ahmed Hany <ahmed.hany@acmesaico.com>
2021-04-02 15:25:40 +0200

    select none if book.shelf is empty

--- src/Book.js
+++ src/Book.js
@@ -12,7 +12,7 @@ class Book extends React.Component {
                     <div className="book-top">
                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}></div>
                         <div className="book-shelf-changer">
-                            <select value={book.shelf} onChange={(event) => onUpdateBook(book, event.target.value)}>
+                            <select value={book.shelf?book.shelf:'none'} onChange={(event) => onUpdateBook(book, event.target.value)}>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
